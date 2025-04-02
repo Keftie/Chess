f2567d27fb585e77259a34be450c7f9f8eb87331
Kevin van den Bosch <kevinvdbosch_1@hotmail.com>
2025-04-02 12:36:05 +0200

    generate allowable moves

--- ChessLogic/Pieces/Pawn.cs
+++ ChessLogic/Pieces/Pawn.cs
@@ -1,13 +1,25 @@
-﻿namespace ChessLogic
+﻿
+namespace ChessLogic
 {
     public class Pawn : Piece
     {
         public override PieceType Type => PieceType.Pawn;
         public override Player Color { get; }
 
+        private readonly Direction forward;
+
         public Pawn(Player color)
         {
             Color = color;
+
+            if (color == Player.White)
+            {
+                forward = Direction.North;
+            }
+            else if (color == Player.Black)
+            {
+                forward = Direction.South;
+            }
         }
 
         public override Piece Copy()
@@ -15,6 +27,54 @@
             Pawn copy = new Pawn(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private static bool CanMoveTo(Position pos, Board board)
+        {
+            return Board.IsInside(pos) && board.IsEmpty(pos);
+        }
+
+        private bool CanCaptureAt(Position pos, Board board)
+        {
+            if (!Board.IsInside(pos) || board.IsEmpty(pos))
+            {
+                return false;
+            }
+
+            return board[pos].Color != Color;
+        }
+
+        private IEnumerable<Move> ForwardMoves(Position from, Board board)
+        {
+            Position oneMovePos = from + forward;
+            if (CanMoveTo(oneMovePos, board))
+            {
+                yield return new NormalMove(from, oneMovePos);
+
+                Position twoMovesPosition = oneMovePos + forward;
+                if (!HasMoved && CanMoveTo(twoMovesPosition, board))
+                {
+                    yield return new NormalMove(from, twoMovesPosition);
+                }
+            }
+        }
+
+        private IEnumerable<Move> DiagonalMoves(Position from, Board board)
+        {
+            foreach (Direction dir in new Direction[] { Direction.West, Direction.East })
+            {
+                Position to = from + forward + dir;
+                if (CanCaptureAt(to, board))
+                {
+                    yield return new NormalMove(from, to);
+                }
+            }
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return ForwardMoves(from, board)
+                .Concat(DiagonalMoves(from, board));
         }
     }
 }
--- ChessLogic/Pieces/Queen.cs
+++ ChessLogic/Pieces/Queen.cs
@@ -5,6 +5,18 @@
         public override PieceType Type => PieceType.Queen;
         public override Player Color { get; }
 
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North,
+            Direction.South,
+            Direction.East,
+            Direction.West,
+            Direction.NorthEast,
+            Direction.NorthWest,
+            Direction.SouthEast,
+            Direction.SouthWest
+        };
+
         public Queen(Player color)
         {
             Color = color;
@@ -15,6 +27,12 @@
             Queen copy = new Queen(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositionsInDir(from, board, dirs)
+                .Select(to => new NormalMove(from, to));
         }
     }
 }
